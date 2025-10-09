9c5a54a0639d233b91da0058e0f845888652ca65
HuyLuan21 <nguyenluan03122005@gmail.com>
2025-10-09 21:02:41 +0700

    school/testing

--- src/js/default.js
+++ src/js/default.js
@@ -1,8 +1,30 @@
- // Responsive nav toggle
- document.addEventListener('DOMContentLoaded', function() {
-    const toggle = document.querySelector('.header__toggle');
-    const nav = document.querySelector('.header__nav');
-    toggle.addEventListener('click', function() {
-        nav.classList.toggle('header__nav--open');
-    });
-});
+// Responsive nav toggle
+document.addEventListener('DOMContentLoaded', function () {
+    const toggle = document.querySelector('.header__toggle')
+    const nav = document.querySelector('.header__nav')
+    toggle.addEventListener('click', function () {
+        nav.classList.toggle('header__nav--open')
+    })
+})
+
+// Tab Navigation
+document.addEventListener('DOMContentLoaded', function () {
+    const items = document.querySelectorAll('.content_item')
+    const tabItems = document.querySelectorAll('.Tab_Navigation_Item')
+
+    items[0].classList.remove('hidden')
+    tabItems[0].classList.add('active')
+
+    tabItems.forEach(item => {
+        item.addEventListener('click', () => {
+            items.forEach(i => i.classList.add('hidden'))
+            items[item.dataset.index].classList.remove('hidden')
+        })
+    })
+    tabItems.forEach(item => {
+        item.addEventListener('click', () => {
+            tabItems.forEach(i => i.classList.remove('active'))
+            item.classList.add('active')
+        })
+    })
+})
