0c492bf650f44b1a668b2cefff7459462ccfea4c
Duong <duominznguyen@gmail.com>
2025-11-05 21:00:57 +0700

    feat: add mobile navbar / fix: responsive navbar

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -2,12 +2,15 @@ const mobileMenuButton = document.querySelector('#mobile-menu-btn')
 const mobileMenuCloseButton = document.querySelector('#mobile-menu-close-btn')
 const mobileMenuContainer = document.querySelector('.header__container-mobile')
 const mobileMenuPanel = document.querySelector('.header__container-mobile-panel')
+const mobileProductToggle = document.querySelector('#mobile-product-toggle')
+const mobileProductSubmenu = document.querySelector('#mobile-product-submenu')
 
 console.log(mobileMenuContainer)
 
 document.addEventListener('DOMContentLoaded', () => {
     mobileMenuButton.addEventListener('click', handleOpenMobileMenu)
     mobileMenuCloseButton.addEventListener('click', handleCloseMobileMenu)
+    mobileProductToggle.addEventListener('click', handleToggleMobileProductSubmenu)
 })
 
 function handleOpenMobileMenu() {
@@ -18,4 +21,9 @@ function handleOpenMobileMenu() {
 function handleCloseMobileMenu() {
     mobileMenuContainer.classList.remove('active')
     mobileMenuPanel.classList.remove('show')
+}
+
+function handleToggleMobileProductSubmenu() {
+    mobileProductSubmenu.classList.toggle('active')
+    mobileProductToggle.classList.toggle('active')
 }
