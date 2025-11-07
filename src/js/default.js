ab0f658592381f9db3c0fd4a2d4bc079d8b32fb3
Duong <duominznguyen@gmail.com>
2025-11-07 12:08:02 +0700

    fix: index page header

--- src/js/default.js
+++ src/js/default.js
@@ -1,11 +1,30 @@
-// Responsive nav toggle
-document.addEventListener('DOMContentLoaded', function () {
-    const toggle = document.querySelector('.header__toggle')
-    const nav = document.querySelector('.header__nav')
-    toggle.addEventListener('click', function () {
-        nav.classList.toggle('header__nav--open')
-    })
+const mobileMenuButton = document.querySelector('#mobile-menu-btn')
+const mobileMenuCloseButton = document.querySelector('#mobile-menu-close-btn')
+const mobileMenuContainer = document.querySelector('.header__container-mobile')
+const mobileMenuPanel = document.querySelector('.header__container-mobile-panel')
+const mobileProductToggle = document.querySelector('#mobile-product-toggle')
+const mobileProductSubmenu = document.querySelector('#mobile-product-submenu')
+
+document.addEventListener('DOMContentLoaded', () => {
+    mobileMenuButton.addEventListener('click', handleOpenMobileMenu)
+    mobileMenuCloseButton.addEventListener('click', handleCloseMobileMenu)
+    mobileProductToggle.addEventListener('click', handleToggleMobileProductSubmenu)
 })
+
+function handleOpenMobileMenu() {
+    mobileMenuContainer.classList.add('active')
+    mobileMenuPanel.classList.add('show')
+}
+
+function handleCloseMobileMenu() {
+    mobileMenuContainer.classList.remove('active')
+    mobileMenuPanel.classList.remove('show')
+}
+
+function handleToggleMobileProductSubmenu() {
+    mobileProductSubmenu.classList.toggle('active')
+    mobileProductToggle.classList.toggle('active')
+}
 
 // Tab Navigation
 document.addEventListener('DOMContentLoaded', function () {
@@ -31,9 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.addEventListener('DOMContentLoaded', function () {
     const items = document.querySelectorAll('#question-bank .content_item')
-    const tabItems = document.querySelectorAll(
-        '#question-bank .Tab_Navigation_Item'
-    )
+    const tabItems = document.querySelectorAll('#question-bank .Tab_Navigation_Item')
 
     items[0].classList.remove('hidden')
     tabItems[0].classList.add('active')
